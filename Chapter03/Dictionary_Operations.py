f631e7214d1aa1efc793d8b3d483dcf86f86d9a6
Dan Nixon <dan@dan-nixon.com>
2015-05-17 12:16:44 +0100

    Added more chapter 3 files

--- Chapter03/Dictionary_Operations.py
+++ Chapter03/Dictionary_Operations.py
@@ -1,5 +1,50 @@
 #!/usr/bin/env python
 
-dict_a = {"name": "Keine", "Occupation": "Teacher"}
+dict_a = {"eggs": 1.50, "milk": 1.2, "bacon": 2.99}
+dict_b = {"bread": 2.20, "jam": 4.87}
+dict_c = {}
 
+# String representation
 print str(dict_a)
+print str(dict_b)
+print str(dict_c)
+
+# Number of keys
+print len(dict_a)
+
+# Iteration
+for key, value in dict_b.iteritems():
+    print "%s -> %f" % (key, value)
+
+# Set key
+dict_c["honey"] = 5.32
+print dict_c
+
+# Get value
+print dict_a["bacon"]
+
+# Get value (safe)
+print dict_b.get("bacon")
+
+# Get calue (using default)
+print dict_b.get("bacon", 6.0)
+
+# Remove key
+del dict_a["bacon"]
+print dict_a
+
+# Test for key
+print "bacon" in dict_a
+
+# Get list of items
+print dict_a.items()
+
+# Get list of keys
+print dict_a.keys()
+
+# Get list of items
+print dict_a.values()
+
+# Update dictionary from another
+dict_c.update(dict_b)
+print dict_c
--- Chapter03/Sets_Operations.py
+++ Chapter03/Sets_Operations.py
@@ -2,6 +2,36 @@
 
 set_a = set(range(0, 10, 2))
 set_b = set(range(5, 10))
+set_c = set(range(5, 15))
 
+# String representation
 print str(set_a)
 print str(set_b)
+print str(set_c)
+
+# Number of members
+print len(set_a)
+
+# Membership test
+print 6 in set_a
+
+# Is disjoint
+print set_b.isdisjoint(set_a)
+
+# Is subset
+print set_b <= set_c
+
+# Is superset
+print set_c >= set_b
+
+# Union
+print set_a | set_b
+
+# Intersection
+print set_a & set_b
+
+# Difference
+print set_a - set_b
+
+# Symmetric Difference
+print set_a ^ set_b
