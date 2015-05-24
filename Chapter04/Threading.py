3bc93521ef74d7dfdff1c3baf1f0c65c7b89e9b9
Dan Nixon <dan@dan-nixon.com>
2015-05-24 13:22:24 +0100

    Added sample files for chapter 4

---
+++ Chapter04/Threading.py
@@ -0,0 +1,35 @@
+import logging
+import threading
+import time
+
+# Set up a logger to output to
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s (T:%(thread)d):- %(message)s")
+
+class MessagePrinter(threading.Thread):
+    """
+    A simple class to print messages to the log a a given interval.
+    """
+    
+    def __init__(self, *args, **kwargs):
+        threading.Thread.__init__(self)
+        self._args = args
+        self._kwargs = kwargs
+
+    def run(self):
+        for message in self._args:
+            logging.info(message)
+            time.sleep(self._kwargs.get("delay", 1.0))
+
+
+# Set up two message printer obejcts
+mp1 = MessagePrinter("Hello", "Good day!")
+mp2 = MessagePrinter("A", "B", "C", delay=3)
+
+# Start each of them on their own thread
+mp1.start()
+mp2.start()
+
+# Wait for those threads to exit before exiting the main thread
+mp1.join()
+mp2.join()
