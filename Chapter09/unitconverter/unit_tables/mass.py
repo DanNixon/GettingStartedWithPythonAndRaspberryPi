36dee122fb62bc872b93bcec1a7f27c4bdf113f9
Dan Nixon <dan@dan-nixon.com>
2015-07-12 21:42:01 +0100

    Added more masses

--- Chapter09/unitconverter/unit_tables/mass.py
+++ Chapter09/unitconverter/unit_tables/mass.py
@@ -8,4 +8,22 @@ class mass(UnitTable.UnitTable):
         # Base unit is gram
         self._base_unit = 'g'
 
-        #TODO
+        # Kilogram
+        self._to_base_unit['kg']    = lambda x: x * 1000.0
+        self._from_base_unit['kg']  = lambda x: x / 1000.0
+
+        # Metric Ton
+        self._to_base_unit['t']     = lambda x: x * 1000000.0
+        self._from_base_unit['t']   = lambda x: x / 1000000.0
+
+        # Pound
+        self._to_base_unit['lb']    = lambda x: x / 0.0022046
+        self._from_base_unit['lb']  = lambda x: x * 0.0022046
+
+        # Stone
+        self._to_base_unit['st']    = lambda x: x / 0.00015747
+        self._from_base_unit['st']  = lambda x: x * 0.00015747
+
+        # Ounce
+        self._to_base_unit['oz']    = lambda x: x / 0.035274
+        self._from_base_unit['oz']  = lambda x: x * 0.035274
