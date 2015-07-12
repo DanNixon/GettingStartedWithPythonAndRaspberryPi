ff6a99d1f187e37e2aa65abb35d2d65a2ea43a48
Dan Nixon <dan@dan-nixon.com>
2015-07-12 21:33:28 +0100

    More length units

--- Chapter09/unitconverter/unit_tables/length.py
+++ Chapter09/unitconverter/unit_tables/length.py
@@ -15,3 +15,31 @@ class length(UnitTable.UnitTable):
         # Millimeters
         self._to_base_unit['mm']    = lambda x: x / 1000.0
         self._from_base_unit['mm']  = lambda x: x * 1000.0
+
+        # Kilometers
+        self._to_base_unit['mm']    = lambda x: x / 1000.0
+        self._from_base_unit['mm']  = lambda x: x * 1000.0
+
+        # Micrometers
+        self._to_base_unit['um']    = lambda x: x / 1000000.0
+        self._from_base_unit['um']  = lambda x: x * 1000000.0
+
+        # Miles
+        self._to_base_unit['mi']    = lambda x: x / 0.00062137
+        self._from_base_unit['mi']  = lambda x: x * 0.00062137
+
+        # Yards
+        self._to_base_unit['yd']    = lambda x: x / 1.0936
+        self._from_base_unit['yd']  = lambda x: x * 1.0936
+
+        # Feet
+        self._to_base_unit['ft']    = lambda x: x / 3.2808
+        self._from_base_unit['ft']  = lambda x: x * 3.2808
+
+        # Inches
+        self._to_base_unit['in']    = lambda x: x / 39.370
+        self._from_base_unit['in']  = lambda x: x * 39.370
+
+        # Mils
+        self._to_base_unit['mil']   = lambda x: x / 39370.078740158
+        self._from_base_unit['mil'] = lambda x: x * 39370.078740158
